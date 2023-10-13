bf76dc4d34a259bf5a6c9e28eeee4ed3045ed193
Grandejosh <60234854+Grandejosh@users.noreply.github.com>
2023-10-13 01:24:04 -0500

    usando .env

--- CampusSuperAdmin/common/conexion.php
+++ CampusSuperAdmin/common/conexion.php
@@ -1,15 +1,16 @@
 <?php
-    $envVars = parse_ini_file('.env');
-    $usuario = $envVars['DB_USERNAME'];
-    $pass = $envVars['DB_PASSWORD'];
-    $location = $envVars['DB_LOCATION'];
-    $db_name = $envVars['DB_NAME'];
+$envFile = $_SERVER['DOCUMENT_ROOT'] . '/.env';
+$envVars = parse_ini_file($envFile);
+$DB_USERNAME = $envVars['DB_USERNAME'];
+$pass = $envVars['DB_PASSWORD'];
+$location = $envVars['DB_LOCATION'];
+$db_name = $envVars['DB_NAME'];
+
 
 $servidor="mysql:dbname=".$db_name.";host=".$location;
-                                        $password="";
 
 try{
-    $pdo= new PDO($servidor,$usuario,$password);
+    $pdo= new PDO($servidor,$DB_USERNAME,$pass);
     //fgg echo "Conectado..";
 
 }catch(PDOException $e){ 
--- CampusSuperAdmin/conexion.php
+++ CampusSuperAdmin/conexion.php
@@ -1,9 +1,11 @@
 <?php 
-    $envVars = parse_ini_file('.env');
-    $usuario = $envVars['DB_USERNAME'];
-    $pass = $envVars['DB_PASSWORD'];
-    $location = $envVars['DB_LOCATION'];
-    $db_name = $envVars['DB_NAME'];
+  $envFile = $_SERVER['DOCUMENT_ROOT'] . '/.env';
+  $envVars = parse_ini_file($envFile);
+  $DB_USERNAME = $envVars['DB_USERNAME'];
+  $pass = $envVars['DB_PASSWORD'];
+  $location = $envVars['DB_LOCATION'];
+  $db_name = $envVars['DB_NAME'];
+  
 
-$conexion = new mysqli($location, $usuario, $pass, $db_name); //Unidad local - usuario - password - nombre de base de datos
+$conexion = new mysqli($location, $DB_USERNAME, $pass, $db_name); //Unidad local - usuario - password - nombre de base de datos
 ?>
