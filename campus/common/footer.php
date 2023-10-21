36b61c80f9db15e70cc1f451e888b2cff6ee9424
Grandejosh <60234854+Grandejosh@users.noreply.github.com>
2023-10-21 14:03:36 -0500

    --
    --

--- campus/common/footer.php
+++ campus/common/footer.php
@@ -1,6 +1,6 @@
 <footer class="site-footer"  style="padding: 5px 0px; background-color: #054261;">
     <div class="text-center">
-       Campus Virtual 2.1 | &copy; Desarrollado por YisusWeb | 2022
+       Campus Virtual 2.1 | &copy; Desarrollado por ARACODE | 2023
         <a href="#" class="go-top">
             <i class="fa fa-angle-up"></i>
         </a>
--- validar.php
+++ validar.php
@@ -16,10 +16,19 @@ if (mysqli_num_rows($proceso) == 1){
 
 	while($resultado = mysqli_fetch_array($proceso)){
 
+		$id = $resultado['id'];
+			$nivel=$conexion->query("SELECT NAME FROM model_has_roles JOIN roles ON role_id = roles.id WHERE model_id = '$id'");
+				if(mysqli_num_rows($nivel) >= 1){
+						while($resultado = mysqli_fetch_array($nivel)){
+							$nivel_rol = $resultado['NAME'];
+					}
+			}
+
 		session_start();
+		if($nivel_rol=="admin")$nivel_rol="Administrador";
 		$_SESSION['ID'] = 1; //$resultado['ID'];
-		$_SESSION['Estado'] = true; //= $resultado['Activo'];
-		$_SESSION['Nivel'] = 'Alumno'; //$resultado['Nivel'];
+		$_SESSION['Estado'] = "Activo"; //= $resultado['Activo'];
+		$_SESSION['Nivel'] = 'Administrador'; //$resultado['Nivel'];
 		$_SESSION['DNI'] = $resultado['number'];
 		$_SESSION['Email'] = 'admin@asdas.com';//$resultado['email'];
 		
