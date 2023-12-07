4f8ad0071681fe8df7d7c254ce97f0d1dab27889
Grandejosh <60234854+Grandejosh@users.noreply.github.com>
2023-12-06 23:39:59 -0500

    d
    d

--- campus/common/acadAgregarVideos.php
+++ campus/common/acadAgregarVideos.php
@@ -1,5 +1,5 @@
 <?php
-include ("../conexion.php");
+include("../conexion.php");
 
 $Posicion = $_POST['PosicionVideos'];
 $idThemes = $_POST['idThemes'];
@@ -7,15 +7,12 @@ $Nombre = $_POST['NombreVideos'];
 $Enlace = $_POST['EnlaceVideos'];
 
 $query = "INSERT INTO aca_contents(position, theme_id, description, content, is_file) 
-VALUES ('$Posicion','$idThemes','$Nombre','$Enlace', 0)";
-        $resultado = $conexion->query($query);
+VALUES ('$Posicion', '$idThemes', '$Nombre', '$Enlace', false)";
+$resultado = $conexion->query($query);
 
-if($resultado){
-    header ("Location: ../videosLista.php?id=".$idThemes);
+if ($resultado) {
+    header("Location: ../videosLista.php?id=$idThemes");
+} else {
+    echo "Tenemos un problema";
 }
-else{
-    
-    echo "Tenemos un Problema";
-}
-
 ?>
